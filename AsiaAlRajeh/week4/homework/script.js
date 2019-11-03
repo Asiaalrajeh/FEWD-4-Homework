60799dcd926b0820e869658c93b731a065bfc7d0
Asia <alrajehasia@gmail.com>
2019-11-04 01:22:42 +0300

    HW4

---
+++ AsiaAlRajeh/week4/homework/script.js
@@ -0,0 +1,17 @@
+var num =1755
+if(num%3==0)
+{
+    console.log('Pling');
+}
+
+ if(num%5==0)
+{
+ console.log('Plang');
+}
+
+ if(num%7==0)
+{
+    console.log('Plong');
+}
+else 
+console.log(num);
